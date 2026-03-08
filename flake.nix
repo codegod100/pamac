e0c9a995d3c4421ddfa00cedf9d8e31001e4d9be
nandi <78769380+codegod100@users.noreply.github.com>
2026-03-08 12:14:50 -0700

    Hardcode core/extra repos if parsing fails

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
             sed -i 's|AlpmConfig (string path)|AlpmConfig (string _unused_path)|g' src/alpm_config.vala
             sed -i 's|conf_path = path;|conf_path = GLib.Environment.get_variable("PACMAN_CONF") ?? "/etc/pacman.conf";|g' src/alpm_config.vala
             sed -i 's|siglevel = Alpm.SigLevel.PACKAGE_OPTIONAL |siglevel = Alpm.SigLevel.USE_DEFAULT |g' src/alpm_config.vala
-            sed -i '/parse_file (conf_path);/a \ \ \ \ \ \ \ \ \ \ \ \ siglevel = Alpm.SigLevel.USE_DEFAULT;' src/alpm_config.vala
+            sed -i '/parse_file (conf_path);/a \ \ \ \ \ \ \ \ \ \ \ \ siglevel = Alpm.SigLevel.USE_DEFAULT;\n\ \ \ \ \ \ \ \ \ \ \ \ if (repo_order.length == 0) {\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ var core = new AlpmRepo ("core");\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ core.urls.add ("https://mirrors.kernel.org/archlinux/$repo/os/$arch");\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ repo_order.add ((owned) core);\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ var extra = new AlpmRepo ("extra");\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ extra.urls.add ("https://mirrors.kernel.org/archlinux/$repo/os/$arch");\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ repo_order.add ((owned) extra);\n\ \ \ \ \ \ \ \ \ \ \ \ }' src/alpm_config.vala
 
             # Patch AlpmConfig defaults
             sed -i 's|"/var/lib/pacman/"|GLib.Environment.get_variable("PACMAN_DBPATH") ?? "/var/lib/pacman/"|g' src/alpm_config.vala
